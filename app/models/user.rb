930d95b73137e4d9666b2490f2d90c48fca5c10b
George <gga1007001@gmail.com>
2019-03-19 22:38:53 +0300

    generate API key for new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Assign an API key on create
+  before_create do |user|
+    user.apikey = user.generate_api_key
+  end
+
+  protected
+
+    # Generate a unique API key
+    def generate_api_key
+      loop do
+        token = SecureRandom.base64.tr('+/=', '')
+        break token unless User.exists?(apikey: token)
+      end
+    end
 end
